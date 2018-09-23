5a57a7c4f163dee747661fbfe3fb6c79e481fc8a
leekaka <lika27@163.com>
2018-09-23 19:32:24 +0800

    增加修改

--- gradient_descent/linear_bgd.py
+++ gradient_descent/linear_bgd.py
@@ -31,7 +31,7 @@ weight = np.random.random(num_input+1)
 
 rate = 0.07
 start = time.clock()
-for epoch in range(0, 10000):                     # 计算loss
+for epoch in range(0, 5000):                     # 计算loss
     predictY = np.zeros((len(x_train)))
     for i in range(0, len(x_train)):
         predictY[i] = np.dot(x_train[i], weight[0:num_input]) + weight[num_input]
@@ -39,12 +39,7 @@ for epoch in range(0, 10000):                     # 计算loss
     for i in range(0, len(x_train)):
         loss += (predictY[i]-y_train[i]) ** 2
 
-    print("epoch: %d-loss: %f" % (epoch, loss))    # 打印迭代次数和损失函数
-    if loss < 0.05:
-        end = time.clock()
-        print("收敛时间:%s ms" % str(end-start))
-        print("迭代次数:%d-epoch" % epoch)
-        break
+    # print("epoch: %d-loss: %f" % (epoch, loss))    # 打印迭代次数和损失函数
     # 计算梯度并更新
     for i in range(0, len(weight)-1):                               # 权重w
         grade = 0
@@ -56,4 +51,8 @@ for epoch in range(0, 10000):                     # 计算loss
         grade += (predictY[j]-y_train[j])
     weight[num_input] = weight[num_input] - rate*grade
 
+
+end = time.clock()
+print("迭代时间:%s ms" % str(end-start))
+print("epoch: %d-loss: %f" % (epoch, loss))
 print(weight)
--- gradient_descent/linear_sgd.py
+++ gradient_descent/linear_sgd.py
@@ -14,66 +14,56 @@ num_input = 5
 
 # 训练参数
 np.random.seed(0)
-normalRand = np.random.normal(0,0.1,sample)      # 10个均值为0方差为0.1 的随机数  (b)
-weight = [7,99,-1,-333,0.06]                     # 1 * 5 权重
-x_train = np.random.random((sample, num_input))  #x 数据（10 * 5）
-y_train = np.zeros((sample,1))                   # y数据（10 * 1）
+b = np.random.normal(0, 0.1, sample)             # 10个均值为0方差为0.1 的随机数  (b)
+w = [7, 85, -10, -600, 20]                 # 1 * 5 权重
+x_train = np.random.random((sample, num_input))  # x 数据（10 * 5）
+y_train = np.zeros((sample, 1))                  # y数据（10 * 1）
 
+for i in range(0, len(x_train)):
+    total = 0
+    for j in range(0, len(x_train[i])):
+        total += w[j]*x_train[i, j]
+    y_train[i] = total + b[i]
 
-for i in range (0,len(x_train)):
-	total = 0
-	for j in range(0,len(x_train[i])):
-		total += weight[j]*x_train[i,j]
-	y_train[i] = total+ normalRand[i]
-
-
-# 训练
+# 训练w
 np.random.seed(0)
 weight = np.random.random(num_input+1)
-rate = 0.04
-batch = 3
+rate = 0.07
+batch = 4
 
-def train(x_train,y_train):
-    #计算损失
-    global weight,rate
-    predictY = np.zeros((len(x_train)))
-    for i in range(0,len(x_train)):
-        predictY[i] = np.dot(x_train[i],weight[0:num_input])+ weight[num_input]
-        loss = 0
-    for i in range(0,len(x_train)):
-        loss += (predictY[i]-y_train[i])**2
 
-    for i in range(0,len(weight)-1):
+def train(x, y):
+    # 计算损失
+    global weight, rate
+    predict_y = np.zeros((len(x)))
+    for i_ in range(0, len(x)):
+        predict_y[i_] = np.dot(x[i_], weight[0:num_input]) + weight[num_input]
+    ls = 0
+    for i_ in range(0, len(x)):
+        ls += (predict_y[i_] - y[i_])**2
+        
+    for i_ in range(0, len(weight)-1):
         grade = 0
-        for j in range(0,len(x_train)):
-            grade += 2*(predictY[j]-y_train[j])*x_train[j,i]
-        weight[i] = weight[i] - rate*grade
-
+        for j_ in range(0, len(x)):
+            grade += 2*(predict_y[j_] - y[j_])*x[j_, i_]
+        weight[i_] = weight[i_] - rate*grade
     grade = 0
-    for j in range(0,len(x_train)):
-        grade += 2*(predictY[j]-y_train[j])
+    for j_ in range(0, len(x)):
+        grade += 2*(predict_y[j_] - y[j_])
         weight[num_input] = weight[num_input] - rate*grade
-
-    return loss
-
-start = time.clock()
-for epoch in range(0,1000):
-     begin = 0
-     while begin < len(x_train):
-          end = begin + batch
-          if end > len(x_train):
-               end = len(x_train)
-
-          loss = train(x_train[begin:end],y_train[begin:end])
-
-          begin = end
-
-          print("epoch: %d-loss: %f"%(epoch,loss))      #打印迭代次数和损失函数
-          if loss < 0.1:
-              end = time.clock()
-              print("收敛时间：%s ms"%str(end-start))
-              print("收敛成功%d-epoch"%epoch)
-              break
+    return ls
 
 
+s = time.time()
+for epoch in range(0, 5000):
+    begin = 0
+    while begin < len(x_train):
+        end = begin + batch
+        if end > len(x_train):
+            end = len(x_train)
+        loss = train(x_train[begin:end], y_train[begin:end])
+        begin = end
+e = time.time()
+print("迭代时间：%s ms" % str(e - s))
+print("epoch: %d-loss: %f" % (epoch, loss))
 print(weight)
